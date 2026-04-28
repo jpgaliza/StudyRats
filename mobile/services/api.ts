652f4ad90ea04eaa574d495d6516bc2e0100c5ec
Júlio Alencar <jsalencar24@gmail.com>
2026-04-28 19:27:41 -0300

    feat: added college IP address

--- mobile/services/api.ts
+++ mobile/services/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const api = axios.create({
-  baseURL: "http://192.168.0.19:8000/api",
+  baseURL: "http://172.16.17.104:8000/api",
 });
 
 api.interceptors.request.use(async (config) => {
